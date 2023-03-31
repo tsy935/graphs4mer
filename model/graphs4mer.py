Add icbeb dataset and fix mae calculation

model/graphs4mer.py
@@ -10,9 +10,20 @@ from torch_geometric.nn import (
 from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool
 import torch_geometric
 import scipy
+import math
 from model.graph_learner import *
 from model.s4 import S4Model
 from model.decoders import SequenceDecoder
+
+def calculate_cosine_decay_weight(max_weight, epoch, epoch_total, min_weight=0):
+    """
+    Calculate decayed weight (hyperparameter) based on cosine annealing schedule
+    Referred to https://arxiv.org/abs/1608.03983
+    and https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.CosineAnnealingLR.html
+    """
+    curr_weight = min_weight + \
+        0.5 * (max_weight - min_weight) * (1 + math.cos(epoch / epoch_total  * math.pi))
+    return curr_weight
 
 def calculate_normalized_laplacian(adj):
     """
@@ -131,7 +142,6 @@ class GraphS4mer(nn.Module):
         channels=1,
         temporal_model="s4",
         bidirectional=False,
-        temporal_pool="mean",
         prenorm=False,
         postact=None,
         metric="self_attention",
@@ -141,19 +151,21 @@ class GraphS4mer(nn.Module):
         prune_method="thresh",
         edge_top_perc=0.5,
         thresh=None,
+        temporal_pool="mean",
         graph_pool="sum",
-        activation_fn="leaky_relu",
+        activation_fn="relu",
         num_classes=1,
         undirected_graph=True,
         use_prior=False,
         K=3,
         regularizations=["feature_smoothing", "degree", "sparse"],
         residual_weight=0.0,
+        decay_residual_weight=False,
         **kwargs
     ):
         super().__init__()
 
-        if max_seq_len % resolution != 0:
+        if (resolution is not None) and (max_seq_len % resolution != 0):
             raise ValueError("max_seq_len must be divisible by resolution!")
 
         self.input_dim = input_dim
@@ -174,6 +186,7 @@ class GraphS4mer(nn.Module):
         self.resolution = resolution
         self.prune_method = prune_method
         self.thresh = thresh
+        self.decay_residual_weight = decay_residual_weight
 
         # temporal layer
         if temporal_model == "gru":
@@ -251,7 +264,14 @@ class GraphS4mer(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
         self.classifier = nn.Linear(hidden_dim, num_classes)
 
-    def forward(self, data, return_attention=False, lengths=None):
+    def forward(
+        self, 
+        data, 
+        return_attention=False, 
+        lengths=None,
+        epoch=None,
+        epoch_total=None,
+    ):
         """
         Args:
             data: torch geometric data object
@@ -261,36 +281,55 @@ class GraphS4mer(nn.Module):
         num_nodes = self.num_nodes
         _, seq_len, _ = x.shape
         batch_idx = data.batch
+
+        if lengths is not None:
+            lengths = torch.repeat_interleave(lengths, num_nodes, dim=0)
+
         # temporal layer
         if self.temporal_model == "s4":
             x = self.t_model(x, lengths)  # (batch * num_nodes, seq_len, hidden_dim)
         else:
+            if lengths is not None:
+                x = pack_padded_sequence(x, lengths, batch_first=True)
             x, _ = self.t_model(x)
-
-        x = x.view(
-            batch, num_nodes, seq_len, -1
-        )  # (batch, num_nodes, seq_len, hidden_dim)
+            if lengths is not None:
+                x, lengths = pad_packed_sequence(x)
 
         # get output with <resolution> as interval
-        x_tmp = []
-        num_dynamic_graphs = self.max_seq_len // self.resolution
-        for t in range(num_dynamic_graphs):
-            start = t * self.resolution
-            stop = start + self.resolution
-            curr_x = torch.mean(x[:, :, start:stop, :], dim=2)
-            x_tmp.append(curr_x)
-        x_tmp = torch.stack(
-            x_tmp, dim=1
-        )  # (batch, num_dynamic_graphs, num_nodes, hidden_dim)
-        x = x_tmp.reshape(
-            -1, num_nodes, self.hidden_dim
-        )  # (batch * num_dynamic_graphs, num_nodes, hidden_dim)
-        del x_tmp
+        if lengths is None:
+            x = x.view(
+                batch, num_nodes, seq_len, -1
+            )  # (batch, num_nodes, seq_len, hidden_dim)
+            x_tmp = []
+            num_dynamic_graphs = self.max_seq_len // self.resolution
+            for t in range(num_dynamic_graphs):
+                start = t * self.resolution
+                stop = start + self.resolution
+                curr_x = torch.mean(x[:, :, start:stop, :], dim=2)
+                x_tmp.append(curr_x)
+            x_tmp = torch.stack(
+                x_tmp, dim=1
+            )  # (batch, num_dynamic_graphs, num_nodes, hidden_dim)
+            x = x_tmp.reshape(
+                -1, num_nodes, self.hidden_dim
+            )  # (batch * num_dynamic_graphs, num_nodes, hidden_dim)
+            del x_tmp
+        else:  # for variable lengths, mean pool over actual lengths
+            x = torch.stack(
+                [
+                    torch.mean(out[:length, :], dim=0)
+                    for out, length in zip(torch.unbind(x, dim=0), lengths)
+                ],
+                dim=0,
+            )
+            x = x.reshape(batch, num_nodes, -1)  # (batch, num_nodes, hidden_dim)
+            num_dynamic_graphs = 1
 
         # get initial adj
         if self.use_prior:
-            edge_index, edge_weight = data.edge_index, data.edge_attr
-            adj_mat = data.adj_mat
+            adj_mat = torch_geometric.utils.to_dense_adj(
+                edge_index=data.edge_index, batch=data.batch, edge_attr=data.edge_attr
+            )
         else:
             # knn cosine graph
             edge_index, edge_weight, adj_mat = get_knn_graph(
@@ -318,14 +357,28 @@ class GraphS4mer(nn.Module):
             adj_mat = torch.cat([adj_mat] * num_dynamic_graphs * batch, dim=0)
         elif len(adj_mat.shape) == 3 and (adj_mat.shape != attn_weight.shape):
             adj_mat = torch.cat([adj_mat] * num_dynamic_graphs, dim=0)
-
+        
+        # knn graph weight (aka residual weight) decay
+        if self.decay_residual_weight:
+            assert (epoch is not None) and (epoch_total is not None)
+            residual_weight = calculate_cosine_decay_weight(
+                max_weight=self.residual_weight, epoch=epoch, epoch_total=epoch_total, min_weight=0
+            )
+        else:
+            residual_weight = self.residual_weight
+        # add knn graph
         adj_mat = (
-            self.residual_weight * adj_mat + (1 - self.residual_weight) * attn_weight
+            residual_weight * adj_mat + (1 - residual_weight) * attn_weight
         )
 
         # prune graph
         adj_mat = prune_adj_mat(
-            adj_mat, num_nodes, method=self.prune_method, edge_top_perc=self.edge_top_perc, knn=self.K, thresh=self.thresh,
+            adj_mat,
+            num_nodes,
+            method=self.prune_method,
+            edge_top_perc=self.edge_top_perc,
+            knn=self.K,
+            thresh=self.thresh,
         )
 
         # regularization loss
@@ -376,6 +429,7 @@ class GraphS4mer(nn.Module):
             x, _ = torch.max(x, dim=1)
         else:
             raise NotImplementedError
+        feat = x.clone()
 
         # classifier
         x = self.classifier(x)
@@ -388,6 +442,7 @@ class GraphS4mer(nn.Module):
                     batch, num_dynamic_graphs, num_nodes, num_nodes
                 ),
                 adj_mat.reshape(batch, num_dynamic_graphs, num_nodes, num_nodes),
+                feat
             )
         else:
             return x, reg_losses
@@ -593,7 +648,14 @@ class GraphS4mer_Regression(nn.Module):
             mode="last" # "last" equivalent to no pooling when input output seq lengths are the same
         )
 
-    def forward(self, data, return_attention=False):
+    def forward(
+        self, 
+        data, 
+        return_attention=False,
+        lengths=None,
+        epoch=None,
+        epoch_total=None
+    ):
         """
         Args:
             data: torch geometric data object

model/s4.py
@@ -1789,6 +1789,7 @@ class S4Model(nn.Module):
         pool_stride=1,
         pool_expand=1,
         temporal_pool=None,
+        use_lengths=False,
         **kernel_args,
     ):
         super().__init__()
@@ -1833,13 +1834,16 @@ class S4Model(nn.Module):
 
         # Linear decoder
         if add_decoder:
-            self.decoder = SequenceDecoder(
-                d_model=d_model,
-                d_output=d_output,
-                l_output=l_output,
-                use_lengths=False,
-                mode=temporal_pool,
-            )
+            if temporal_pool == 'mean':
+                self.decoder = nn.Linear(d_model, d_output)
+            else:
+                self.decoder = SequenceDecoder(
+                    d_model=d_model,
+                    d_output=d_output,
+                    l_output=l_output,
+                    use_lengths=use_lengths,
+                    mode=temporal_pool,
+                )
 
     def forward(self, x, lengths=None):
         """
@@ -1877,17 +1881,23 @@ class S4Model(nn.Module):
 
         x = x.transpose(-1, -2)  # (B, L, d_model)
 
-        # # Pooling: average pooling over the sequence length
-        # if self.temporal_pool == "mean":
-        #     x = x.mean(dim=1)
-        # elif self.temporal_pool == "last":
-        #     x = x[:, -1, :]
-        # elif self.temporal_pool is None:
-        #     pass
-
         # Decode the outputs
         if self.add_decoder:
-            x = self.decoder(x)  # (B, L, d_model) -> (B, L_out, d_output)
+            if self.temporal_pool == 'mean':
+                if lengths is None:
+                    x = torch.mean(x, dim=1) # (B, d_model)
+                else:
+                    x = torch.stack(
+                        [
+                            torch.mean(out[:length, :], dim=0)
+                            for out, length in zip(torch.unbind(x, dim=0), lengths)
+                        ],
+                        dim=0,
+                    ) # (B, d_model)
+                    x = self.decoder(x)
+            else:
+                x = self.decoder(x)  # (B, L, d_model) -> (B, L_out, d_output)
+
             if x.shape[1] == 1:
                 x = x.squeeze(1)
 

model/temporal_gnn.py
@@ -154,7 +154,7 @@ class TemporalGNN(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
         self.classifier = nn.Linear(hidden_dim, num_classes)
 
-    def forward(self, data):
+    def forward(self, data, lengths=None):
         """
         Args:
             data: torch geometric data object
@@ -164,6 +164,9 @@ class TemporalGNN(nn.Module):
         _, seq_len, _ = x.shape
         num_nodes = self.num_nodes
         batch_idx = data.batch
+
+        if lengths is not None:
+            lengths = torch.repeat_interleave(lengths, num_nodes, dim=0)
 
         if self.use_prior:
             edge_index, edge_weight = data.edge_index, data.edge_attr
@@ -178,18 +181,39 @@ class TemporalGNN(nn.Module):
             edge_index = edge_index.to(x.device)
             edge_weight = edge_weight.to(x.device)
             adj_mat = adj_mat.to(x.device)
+            
+        # add self-loop
+        edge_index, edge_weight = torch_geometric.utils.remove_self_loops(
+            edge_index=edge_index, edge_attr=edge_weight
+        )
+        edge_index, edge_weight = torch_geometric.utils.add_self_loops(
+            edge_index=edge_index,
+            edge_attr=edge_weight,
+            fill_value=1,
+        )
 
         # temporal layer
         if self.temporal_model == "s4":
-            x = self.t_model(x)  # (batch * num_nodes, seq_len, hidden_dim)
+            x = self.t_model(x, lengths=lengths)  # (batch * num_nodes, seq_len, hidden_dim)
         else:
             x, _ = self.t_model(x)
 
         # temporal pool
         if self.temporal_pool == "mean":
-            x = torch.mean(x, dim=1) # (batch * num_nodes, hidden_dim)
+            if lengths is None:
+                x = torch.mean(x, dim=1)  # (batch * num_nodes, hidden_dim)
+            else:
+                x = torch.stack(
+                    [
+                        torch.mean(out[:length, :], dim=0)
+                        for out, length in zip(torch.unbind(x, dim=0), lengths)
+                    ],
+                    dim=0,
+                ) # (batch * num_nodes, hidden_dim)
         else:
-            x = self.temporal_pool_layer(x).squeeze(1)  # (batch * num_nodes, hidden_dim)
+            x = self.temporal_pool_layer(x).squeeze(
+                1
+            )  # (batch * num_nodes, hidden_dim)
 
         for i in range(len(self.gnn_layers)):
 
@@ -215,7 +239,6 @@ class TemporalGNN(nn.Module):
         x = self.classifier(x)
 
         return x
-
 
 class TemporalGNN_Regression(nn.Module):
     def __init__(

utils/utils.py
@@ -11,6 +11,9 @@ from sklearn.metrics import (
     precision_recall_curve,
     accuracy_score,
     roc_auc_score,
+    roc_curve,
+    average_precision_score,
+    confusion_matrix
 )
 from collections import OrderedDict, defaultdict
 from itertools import repeat
@@ -39,6 +42,9 @@ import sys
 import pickle
 import scipy.sparse as sp
 from collections import Counter
+
+import warnings
+warnings.filterwarnings("ignore")
 
 MASK = 0.0
 LARGE_NUM = 1e9
@@ -86,7 +92,16 @@ def get_save_dir(base_dir, training, id_max=500):
                        Delete old save directories or use another name."
     )
 
-def eval_dict(y_pred, y, y_prob=None, average="binary", metrics=[]):
+def eval_dict(
+    y_pred,
+    y,
+    y_prob=None,
+    average="binary",
+    metrics=[],
+    null_val=0.0,
+    thresholds=None,
+    find_threshold_on=None,
+):
     """
     Args:
         y_pred: Predicted labels of all samples
@@ -98,8 +113,22 @@ def eval_dict(y_pred, y, y_prob=None, average="binary", metrics=[]):
         pred_dict: Dictionary containing predictions
         true_dict: Dictionary containing labels
     """
-
     scores_dict = {}
+
+    if find_threshold_on is not None:
+        if find_threshold_on == "gbeta":
+            thresholds = find_optimal_cutoff_thresholds_for_beta(y, y_prob, threshold_on="gbeta")
+        elif find_threshold_on == "fbeta":
+            thresholds = find_optimal_cutoff_thresholds_for_beta(y, y_prob, threshold_on="fbeta")
+        elif find_threshold_on == "F1":
+            thresholds = find_optimal_cutoff_thresholds_for_f1(y, y_prob)
+        elif find_threshold_on == "youden":
+            thresholds = find_optimal_cutoff_thresholds_youden(y, y_prob)
+        else:
+            raise NotImplementedError
+
+    if thresholds is not None:
+        y_pred = apply_thresholds(y_prob, thresholds)
 
     if "acc" in metrics:
         scores_dict["acc"] = accuracy_score(y_true=y, y_pred=y_pred)
@@ -113,23 +142,43 @@ def eval_dict(y_pred, y, y_prob=None, average="binary", metrics=[]):
         scores_dict["recall"] = recall_score(y_true=y, y_pred=y_pred, average=average)
     if "auroc" in metrics:
         assert y_prob is not None
-        if len(np.unique(y)) <= 2:  # binary case
-            scores_dict["auroc"] = roc_auc_score(y_true=y, y_score=y_prob)
-        else:  # multiclass/multilabel case
-            scores_dict["auroc"] = roc_auc_score(
-                y_true=y, y_score=y_prob, average=average, multi_class="ovr"
-            )
+        scores_dict["auroc"] = roc_auc_score(
+            y_true=y, y_score=y_prob, average=average if (average!="binary") else None,
+        )
+    if "aupr" in metrics:
+        assert y_prob is not None
+        scores_dict["aupr"] = average_precision_score(
+            y_true=y, 
+            y_score=y_prob, 
+            average=average if (average!="binary") else None,
+        )
+    if "specificity" in metrics:
+        tn, fp, fn, tp = confusion_matrix(y_true=y, y_pred=y_pred).ravel()
+        scores_dict["specificity"] = tn / (tn + fp)
     if "kappa" in metrics:
         scores_dict["kappa"] = cohen_kappa_score(y1=y, y2=y_pred)
     if "mae" in metrics:
-        scores_dict["mae"] = masked_mae_np(preds=y_pred, labels=y)
+        scores_dict["mae"] = masked_mae_np(preds=y_pred, labels=y, null_val=null_val)
     if "rmse" in metrics:
-        scores_dict["rmse"] = masked_rmse_np(preds=y_pred, labels=y)
+        scores_dict["rmse"] = masked_rmse_np(preds=y_pred, labels=y, null_val=null_val)
     if "mse" in metrics:
-        scores_dict["mse"] = masked_mse_np(preds=y_pred, labels=y)
+        scores_dict["mse"] = masked_mse_np(preds=y_pred, labels=y, null_val=null_val)
     if "mape" in metrics:
-        scores_dict["mape"] = masked_mape_np(preds=y_pred, labels=y)
-    return scores_dict
+        scores_dict["mape"] = masked_mape_np(preds=y_pred, labels=y, null_val=null_val)
+    if ("fbeta" in metrics) or ("gbeta" in metrics):
+        fbeta, gbeta = compute_fbeta_gbeta(
+            y_true=y, y_pred=y_pred
+        )
+        if average == "macro":
+            scores_dict["fbeta"] = np.mean(fbeta)
+            scores_dict["gbeta"] = np.mean(gbeta)
+        elif (average is None): # individual classes
+            scores_dict["fbeta"] = fbeta
+            scores_dict["gbeta"] = gbeta
+        else:
+            raise NotImplementedError
+
+    return scores_dict, thresholds
 
 
 def masked_rmse_np(preds, labels, null_val=np.nan):
@@ -181,11 +230,11 @@ def masked_mape_np(preds, labels, null_val=np.nan):
         mape = np.nan_to_num(mask * mape)
         return np.mean(mape)
 
-def masked_mae_loss(y_pred, y_true, mask_val=0.0):
+def masked_mae_loss(y_pred, y_true, null_val=0.0):
     """
     Only compute loss on unmasked part
     """
-    masks = (y_true != mask_val).float()
+    masks = (y_true != null_val).float()
     masks /= masks.mean()
     loss = torch.abs(y_pred - y_true)
     loss = loss * masks
@@ -195,11 +244,11 @@ def masked_mae_loss(y_pred, y_true, mask_val=0.0):
     return loss.mean()
 
 
-def masked_mse_loss(y_pred, y_true, mask_val=0.0):
+def masked_mse_loss(y_pred, y_true, null_val=0.0):
     """
     Only compute MSE loss on unmasked part
     """
-    masks = (y_true != mask_val).float()
+    masks = (y_true != null_val).float()
     masks /= masks.mean()
     loss = (y_pred - y_true).pow(2)
     loss = loss * masks
@@ -247,3 +296,133 @@ def compute_regression_loss(
         return masked_mae_loss(y_predicted, y_true, mask_val=mask_val)
     else:
         return masked_mse_loss(y_predicted, y_true, mask_val=mask_val)
+
+
+""" Metrics for ICBEB ECG dataset from https://github.com/helme/ecg_ptbxl_benchmarking/blob/master/code/utils/utils.py """
+
+
+def find_optimal_cutoff_threshold_for_beta(target, predicted, n_thresholds=100, threshold_on="gbeta"):
+    thresholds = np.linspace(0.00, 1, n_thresholds)
+    scores = [
+        challenge_metrics(target, predicted > t, single=True)["G_beta_macro"]
+        for t in thresholds
+    ]
+    optimal_idx = np.argmax(scores)
+    return thresholds[optimal_idx]
+
+
+def find_optimal_cutoff_thresholds_for_beta(y_true, y_pred, threshold_on="gbeta"):
+    return [
+        find_optimal_cutoff_threshold_for_beta(
+            y_true[:, k][:, np.newaxis], y_pred[:, k][:, np.newaxis], threshold_on=threshold_on
+        )
+        for k in range(y_true.shape[1])
+    ]
+
+def thresh_max_f1(y_true, y_prob):
+    """
+    Find best threshold based on precision-recall curve to maximize F1-score.
+    Binary calssification only
+    """
+    if len(np.unique(y_true)) > 2:
+        raise NotImplementedError
+
+    precision, recall, thresholds = precision_recall_curve(y_true, y_prob)
+    thresh_filt = []
+    fscore = []
+    n_thresh = len(thresholds)
+    for idx in range(n_thresh):
+        curr_f1 = (2 * precision[idx] * recall[idx]) / (precision[idx] + recall[idx])
+        if not (np.isnan(curr_f1)):
+            fscore.append(curr_f1)
+            thresh_filt.append(thresholds[idx])
+    # locate the index of the largest f score
+    ix = np.argmax(np.array(fscore))
+    best_thresh = thresh_filt[ix]
+    return best_thresh
+
+def find_optimal_cutoff_thresholds_for_f1(y_true, y_prob):
+    return [
+        thresh_max_f1(
+            y_true[:, k][:, np.newaxis], y_prob[:, k][:, np.newaxis]
+        )
+        for k in range(y_true.shape[1])
+    ]
+
+def thresh_youden(y_true, y_prob):
+    """Determine cutoff threshold based on Youden's J-Index"""
+    fpr, tpr, thresholds = roc_curve(y_true, y_prob)
+    youdenJ = tpr - fpr
+    optimal_idx = np.argmax(youdenJ)
+    optimal_thresh = thresholds[optimal_idx]
+    return optimal_thresh
+
+def find_optimal_cutoff_thresholds_youden(y_true, y_prob):
+    return [
+        thresh_youden(
+            y_true[:, k][:, np.newaxis], y_prob[:, k][:, np.newaxis]
+        )
+        for k in range(y_true.shape[1])
+    ]
+
+
+def apply_thresholds(preds, thresholds):
+    """
+    apply class-wise thresholds to prediction score in order to get binary format.
+    BUT: if no score is above threshold, pick maximum. This is needed due to metric issues.
+    """
+    tmp = []
+    for p in preds:
+        tmp_p = (p > thresholds).astype(int)
+        if np.sum(tmp_p) == 0:
+            tmp_p[np.argmax(p)] = 1
+        tmp.append(tmp_p)
+    tmp = np.array(tmp)
+    return tmp
+
+
+def challenge_metrics(
+    y_true, y_pred, beta1=2, beta2=2, class_weights=None, single=False
+):
+    """Adapted from https://github.com/helme/ecg_ptbxl_benchmarking/blob/master/code/utils/utils.py"""
+    f_beta = 0
+    g_beta = 0
+    f_beta_all = []
+    g_beta_all = []
+    for classi in range(y_true.shape[1]):
+        y_truei, y_predi = y_true[:, classi], y_pred[:, classi]
+        TP, FP, TN, FN = 0.0, 0.0, 0.0, 0.0
+        for i in range(len(y_predi)):
+            if y_truei[i] == y_predi[i] == 1:
+                TP += 1.0
+            if (y_predi[i] == 1) and (y_truei[i] != y_predi[i]):
+                FP += 1.0
+            if y_truei[i] == y_predi[i] == 0:
+                TN += 1.0
+            if (y_predi[i] == 0) and (y_truei[i] != y_predi[i]):
+                FN += 1.0
+        f_beta_i = ((1 + beta1**2) * TP) / (
+            (1 + beta1**2) * TP + FP + (beta1**2) * FN
+        )
+        g_beta_i = (TP) / (TP + FP + beta2 * FN)
+
+        f_beta += f_beta_i
+        g_beta += g_beta_i
+
+        f_beta_all.append(f_beta_i)
+        g_beta_all.append(g_beta_i)
+
+    return {
+        "F_beta_macro": f_beta / y_true.shape[1],
+        "G_beta_macro": g_beta / y_true.shape[1],
+        "F_beta_all": np.array(f_beta_all),
+        "G_beta_all": np.array(g_beta_all),
+    }
+
+def compute_fbeta_gbeta(y_true, y_pred):
+    # PhysioNet/CinC Challenges metrics
+    challenge_scores = challenge_metrics(y_true, y_pred, beta1=2, beta2=2)
+    fbeta = challenge_scores["F_beta_all"]
+    gbeta = challenge_scores["G_beta_all"]
+
+    return fbeta, gbeta